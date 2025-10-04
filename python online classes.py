d3074aa2fcb4afa046b3396eb42c5f45bbaeee88
jeremiahobasi10-dev <jeremiahobasi10@gmail.com>
2025-10-04 05:35:01 -0700

    Add files via upload
    uploaded version 1

---
+++ python online classes.py
@@ -0,0 +1,5 @@
+# import random as rand
+# items = ["iphone","samsung","nothing","bag and pencil"]
+# bias = [10,9,1000,200]
+# your_choice = rand.choices(items,weights=bias,k=1)
+# print(your_choice)
